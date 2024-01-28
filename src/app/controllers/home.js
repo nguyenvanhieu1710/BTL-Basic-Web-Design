7b6b286bcbbd2c1a35e93574e814992539d674fb
Nguyen Van Hieu <hieunghiendo1@gmail.com>
2024-01-28 09:39:47 +0700

    Update code

---
+++ src/app/controllers/home.js
@@ -0,0 +1,10 @@
+var search = document.getElementById("Search");
+function clicked() {
+  //   search.innerHTML = " ";
+  //   console.log(search.innerHTML);
+  search.classList.remove("displayNone");
+}
+
+function Dong() {
+  search.classList.add("displayNone");
+}
